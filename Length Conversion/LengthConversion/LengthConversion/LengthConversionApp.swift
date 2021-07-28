3198ec25a2681caac627d78d003de04a50d1bcb8
Lucas Mendes <lucas.augustus@gmail.com>
2021-07-27 21:45:20 -0300

    added project length conversion

---
+++ Length Conversion/LengthConversion/LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Lucas Mendes on 27/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
