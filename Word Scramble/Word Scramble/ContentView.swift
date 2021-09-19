0b728d552cad3b3f305118b6cdb2a4b8299bf6a5
Lucas Augusto Mendes Rodrigues <lucas.augusto@avenue.us>
2021-09-19 13:47:02 -0300

    Added new project

---
+++ Word Scramble/Word Scramble/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by Lucas Augusto on 19/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Section(header: Text("Section 1")) {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+
+            Section(header: Text("Section 2")) {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+
+            Section(header: Text("Section 3")) {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        }
+        .listStyle(GroupedListStyle())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Lucas Augusto on 19/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
