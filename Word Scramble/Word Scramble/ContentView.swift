6099f4f24211825ce29a3b870aab113dc689c6db
Lucas Augusto Mendes Rodrigues <lucas.augustus@gmail.com>
2021-09-19 14:17:12 -0300

    Added states

--- Word Scramble/Word Scramble/ContentView.swift
+++ Word Scramble/Word Scramble/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
         List {
             Section(header: Text("Section 1")) {
